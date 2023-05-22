c2915fa7c78c9f46636595361ef401646de5f18a
lucas.romanato <lucas.romanato@mindlog.com.br>
2023-05-22 10:00:47 -0300

    adicionando update e delete

--- backend/src/controllers/HouseController.js
+++ backend/src/controllers/HouseController.js
@@ -1,6 +1,14 @@
 import House from "../models/House";
 
 class HouseController {
+  async index(req, res) {
+    const { status } = req.query;
+
+    const houses = await House.find({ status });
+
+    res.json(houses);
+  }
+
   async store(req, res) {
     const { filename } = req.file;
     const { description, price, location, status } = req.body;
@@ -16,6 +24,27 @@ class HouseController {
     });
 
     res.json(house);
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+
+    const houses = await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      }
+    );
+
+    res.json(houses);
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ import uploadConfig from "./config/upload";
 
 import SessionController from "./controllers/SessionController";
 import HouseController from "./controllers/HouseController";
+import House from "./models/House";
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -11,5 +12,11 @@ const upload = multer(uploadConfig);
 routes.post("/sessions", SessionController.store);
 
 routes.post("/houses", upload.single("thumbnail"), HouseController.store);
+routes.get("/houses", HouseController.index);
+routes.put(
+  "/houses/:house_id",
+  upload.single("thumbnail"),
+  HouseController.update
+);
 
 export default routes;
